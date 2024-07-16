df8454ee42425b5523b4ebb0c69f485373722499
sazed1237 <sazedulislam9126@gmail.com>
2024-07-16 09:55:43 +0600

    fixed dynamic commit count

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -18,56 +18,116 @@ const Stats = () => {
 
     useEffect(() => {
         const fetchCommits = async () => {
+
             try {
-                const reposResponse = await axios.get(`https://api.github.com/users/${GITHUB_USERNAME}/repos`, {
-                    headers: {
-                        Authorization: `token ${GITHUB_TOKEN}`
-                    }
+                const repos = await fetchAllRepositories();
+
+                const commitCountPromises = repos.map(repo => {
+                    return axios.get(`https://api.github.com/repos/${GITHUB_USERNAME}/${repo.name}/commits?per_page=1`, {
+                        headers: {
+                            Authorization: `token ${GITHUB_TOKEN}`
+                        }
+                    }).then(response => {
+                        const commitCount = response.headers['link']
+                            ? parseInt(response.headers['link'].match(/&page=(\d+)>; rel="last"/)[1])
+                            : response.data.length;
+                        return commitCount;
+                    }).catch(error => {
+                        console.error(`Error fetching commits for ${repo.name}:`, error);
+                        return 0;
+                    });
                 });
-                const repos = reposResponse.data;
-                console.log("repos", repos)
 
+                const commitCounts = await Promise.all(commitCountPromises);
+                const totalCommits = commitCounts.reduce((acc, count) => acc + count, 0);
 
-                let totalCommits = 0;
-                for (const repo of repos) {
-                    try {
-                        const commitsResponse = await fetchAllCommits(repo.name);
-                        totalCommits += commitsResponse.length;
-                    } catch (commitError) {
-                        console.error(`Error fetching commits for ${repo.name}:`, commitError);
-                    }
-                }
-                console.log("total commits", totalCommits)
                 setCommitCount(totalCommits);
             } catch (error) {
                 console.error('Error fetching repositories:', error);
             }
-        };
 
-        const fetchAllCommits = async (repoName) => {
-            let commits = [];
-            let page = 1;
-            while (true) {
-                const response = await axios.get(`https://api.github.com/repos/${GITHUB_USERNAME}/${repoName}/commits`, {
-                    headers: {
-                        Authorization: `token ${GITHUB_TOKEN}`
-                    },
-                    params: {
-                        per_page: 100,
-                        page: page,
-                    }
-                });
-                if (response.data.length === 0) break;
-                commits = commits.concat(response.data);
-                page += 1;
-            }
-            return commits;
+
+
+            // try {
+            //     const allRepos = await fetchAllRepositories()
+            //     console.log("All Repos", allRepos)
+
+
+            //     let totalCommits = 0;
+            //     for (const repo of allRepos) {
+            //         try {
+            //             const commitsResponse = await fetchAllCommits(repo?.name);
+            //             totalCommits += commitsResponse.length;
+            //         } catch (commitError) {
+            //             console.error(`Error fetching commits for ${repo?.name}:`, commitError);
+            //         }
+            //     }
+            //     console.log("total commits", totalCommits)
+            //     setCommitCount(totalCommits);
+            // } catch (error) {
+            //     console.error('Error fetching repositories:', error);
+            // }
         };
 
         fetchCommits();
+
     }, [commitCount]);
 
-    console.log(commitCount)
+
+
+    // all Repositories fetching
+    const fetchAllRepositories = async () => {
+        let page = 1;
+        let allRepos = [];
+
+        while (true) {
+            const reposResponse = await axios.get(`https://api.github.com/users/${GITHUB_USERNAME}/repos`, {
+                headers: {
+                    Authorization: `token ${GITHUB_TOKEN}`
+                },
+                params: {
+                    per_page: 100,
+                    page: page,
+                }
+            });
+
+            if (reposResponse.data.length === 0) {
+                break;
+            }
+
+            allRepos = allRepos.concat(reposResponse.data);
+            page += 1
+
+        }
+        // console.log('all repositories', allRepos)
+
+        return allRepos;
+    }
+
+
+
+    // all commits fetching
+    // const fetchAllCommits = async (repoName) => {
+    //     let commits = [];
+    //     let page = 1;
+    //     while (true) {
+    //         const response = await axios.get(`https://api.github.com/repos/${GITHUB_USERNAME}/${repoName}/commits`, {
+    //             headers: {
+    //                 Authorization: `token ${GITHUB_TOKEN}`
+    //             },
+    //             params: {
+    //                 per_page: 100,
+    //                 page: page,
+    //             }
+    //         });
+    //         if (response.data.length === 0) break;
+    //         commits = commits.concat(response.data);
+    //         page += 1;
+    //     }
+    //     return commits;
+    // };
+
+
 
     const statsItems = [
         {
@@ -87,6 +147,7 @@ const Stats = () => {
             text: "Code commits",
         },
     ]
+
 
     return (
         <section className='pt-4 pb-12 lg:pt-0 '>
