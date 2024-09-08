5710ff5a249dde534a86200870dab6e7888fbb34
sazed1237 <sazedulislam9126@gmail.com>
2024-09-08 22:44:45 +0600

    project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,6 +12,9 @@ import project10 from '../assets/projects/carDoctor.png'
 import project11 from '../assets/projects/coffeStoreManagement.png'
 import project12 from '../assets/projects/theNewSdragon.png'
 import project13 from '../assets/projects/emaJohn.png'
+import project14 from '../assets/projects/cryptoSteps.png'
+import project15 from '../assets/projects/globalchain.png'
+import project16 from '../assets/projects/realestate.png'
 
 
 export const projects = [
@@ -21,12 +24,42 @@ export const projects = [
         title: "Helps Electronics",
         description: "Developed a full-stack e-commerce website using the MERN stack, Tailwind CSS, and Mongoose. Key features include user authentication, admin role management, product management, and an add-to-cart functionality. This project showcases comprehensive capabilities in both front-end and back-end development, ensuring a seamless shopping experience for users.",
         stack: [{ name: "react.js" }, { name: "tailwind css" }, { name: "javascript" }, { name: "node.js" }, { name: "express.js" }, { name: "mongoDB" }, { name: "mongoose" }, { name: "E-Commerce" },],
-        thumb: project1,
+        thumb: project14,
         live: "https://helps-electronics-frontend.vercel.app/",
         github: "https://github.com/sazed1237?tab=repositories&q=helps-electronics&type=&language=&sort=",
     },
     {
         num: "02",
+        category: "Dashboard",
+        title: "Crypto Steps",
+        description: "I have created a trade journal dashboard. here you can add your trade details and monitoring this in many of ways",
+        stack: [{ name: "react.js" }, { name: "tailwind css" }, { name: "javascript" }, { name: "node.js" }, { name: "express.js" }, { name: "mongoDB" }, { name: "mongoose" }, { name: "E-Commerce" }, { name: "Firebase" },],
+        thumb: project14,
+        live: "https://crypto-steps-51054.web.app/",
+        github: "https://github.com/sazed1237?tab=repositories&q=crypto-steps&type=&language=&sort=",
+    },
+    {
+        num: "03",
+        category: "Frontend",
+        title: "Real estate",
+        description: "Developed a comprehensive restaurant management website using the MERN stack, Tailwind CSS, and Firebase. The project features a complete user interface for customers, as well as a robust dashboard for both users and admins. Admin roles include full administrative capabilities. This project demonstrates expertise in creating complex, role-based web applications.",
+        stack: [{ name: "react.js" }, { name: "tailwind css" }, { name: "javascript" },],
+        thumb: project16,
+        live: "https://builder-manage.netlify.app/",
+        github: "https://github.com/sazed1237?tab=repositories&q=builder-manage&type=&language=&sort=",
+    },
+    {
+        num: "04",
+        category: "Full Stack",
+        title: "Global Chain Academy",
+        description: "Developed a comprehensive restaurant management website using the MERN stack, Tailwind CSS, and Firebase. The project features a complete user interface for customers, as well as a robust dashboard for both users and admins. Admin roles include full administrative capabilities. This project demonstrates expertise in creating complex, role-based web applications.",
+        stack: [{ name: "react.js" }, { name: "tailwind css" }, { name: "javascript" }, { name: "node.js" }, { name: "express.js" }, { name: "mongoDB" }, { name: "firebase" }, { name: "E-Commerce" },],
+        thumb: project15,
+        live: "https://globalchainacademy.web.app/",
+        github: "https://github.com/sazed1237?tab=repositories&q=global-academy&type=&language=&sort=",
+    },
+    {
+        num: "05",
         category: "Full Stack",
         title: "Bistro Boss",
         description: "Developed a comprehensive restaurant management website using the MERN stack, Tailwind CSS, and Firebase. The project features a complete user interface for customers, as well as a robust dashboard for both users and admins. Admin roles include full administrative capabilities. This project demonstrates expertise in creating complex, role-based web applications.",
@@ -36,7 +69,7 @@ export const projects = [
         github: "https://github.com/sazed1237?tab=repositories&q=bistro-boss&type=&language=&sort=",
     },
     {
-        num: "03",
+        num: "06",
         category: "Frontend",
         title: "Hotel Villa",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta velit consectetur ex veritatis. Obcaecati quibusdam explicabo ullam et labore quo.",
@@ -46,7 +79,7 @@ export const projects = [
         github: "https://github.com/sazed1237?tab=repositories&q=hotel-villa&type=&language=&sort=",
     },
     {
-        num: "04",
+        num: "07",
         category: "Full Stack",
         title: "Beauty Parlor",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta velit consectetur ex veritatis. Obcaecati quibusdam explicabo ullam et labore quo.",
@@ -56,7 +89,7 @@ export const projects = [
         github: "https://github.com/sazed1237?tab=repositories&q=beauty-parlor&type=&language=&sort=",
     },
     {
-        num: "05",
+        num: "08",
         category: "Frontend",
         title: "Sazed Movie Hut",
         description: "Created a dynamic movie and TV show website using the MERN stack, Tailwind CSS, and The Movie Database (TMDB) API. The platform allows users to watch movies and TV series, providing detailed information on each title. This project highlights proficiency in integrating live APIs and developing interactive, user-friendly interfaces.",
@@ -66,7 +99,7 @@ export const projects = [
         github: "https://github.com/sazed1237?tab=repositories&q=sazed-movie-hut&type=&language=&sort=",
     },
     {
-        num: "06",
+        num: "09",
         category: "Full Stack",
         title: "SIR Eco",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta velit consectetur ex veritatis. Obcaecati quibusdam explicabo ullam et labore quo.",
@@ -76,7 +109,7 @@ export const projects = [
         github: "https://github.com/sazed1237?tab=repositories&q=sir-eco&type=&language=&sort=",
     },
     {
-        num: "07",
+        num: "10",
         category: "Full Stack",
         title: "The Dragon News",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta velit consectetur ex veritatis. Obcaecati quibusdam explicabo ullam et labore quo.",
@@ -86,7 +119,7 @@ export const projects = [
         github: "https://github.com/sazed1237?tab=repositories&q=the-dragon-news-next&type=&language=&sort=",
     },
     {
-        num: "08",
+        num: "11",
         category: "Frontend",
         title: "Helpest BD",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta velit consectetur ex veritatis. Obcaecati quibusdam explicabo ullam et labore quo.",
@@ -96,7 +129,7 @@ export const projects = [
         github: "https://github.com/sazed1237?tab=repositories&q=helpest&type=&language=&sort=",
     },
     {
-        num: "09",
+        num: "12",
         category: "Full Stack",
         title: "Doctors Portal",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta velit consectetur ex veritatis. Obcaecati quibusdam explicabo ullam et labore quo.",
@@ -106,7 +139,7 @@ export const projects = [
         github: "https://github.com/sazed1237?tab=repositories&q=doctors-portal&type=&language=&sort=",
     },
     {
-        num: "10",
+        num: "13",
         category: "Full Stack",
         title: "Car Doctor",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta velit consectetur ex veritatis. Obcaecati quibusdam explicabo ullam et labore quo.",
@@ -116,7 +149,7 @@ export const projects = [
         github: "https://github.com/sazed1237?tab=repositories&q=car-doctor&type=&language=&sort=",
     },
     {
-        num: "11",
+        num: "14",
         category: "Frontend",
         title: "Coffee Store Management",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta velit consectetur ex veritatis. Obcaecati quibusdam explicabo ullam et labore quo.",
@@ -126,7 +159,7 @@ export const projects = [
         github: "https://github.com/sazed1237?tab=repositories&q=Espresso-emporium&type=&language=&sort=",
     },
     {
-        num: "12",
+        num: "15",
         category: "Full Stack",
         title: "The News Dragon",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta velit consectetur ex veritatis. Obcaecati quibusdam explicabo ullam et labore quo.",
@@ -136,7 +169,7 @@ export const projects = [
         github: "https://github.com/sazed1237?tab=repositories&q=the-news-dragon-&type=&language=&sort=",
     },
     {
-        num: "13",
+        num: "16",
         category: "Frontend",
         title: "Ema John",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta velit consectetur ex veritatis. Obcaecati quibusdam explicabo ullam et labore quo.",
